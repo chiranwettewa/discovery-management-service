17182ca468655fcb08a47efb051184f4c27c95cc
chiranwettewa <chiranwettewa@gmail.com>
2022-12-12 16:42:57 +0530

    init

--- src/main/java/com/chiran/discoverymanagementservice/DiscoveryManagementServiceApplication.java
+++ src/main/java/com/chiran/discoverymanagementservice/DiscoveryManagementServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryManagementServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryManagementServiceApplication.class, args);
 	}
